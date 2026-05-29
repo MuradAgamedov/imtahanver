fb7ed31943d4dc7dbad2bf950fe90650859d0bf1
murad agamedov <agamedov94@mail.ru>
2026-05-29 17:18:05 +0400

    applciant subject seeder

---
+++ backend/database/seeders/ApplicantSubjectSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ApplicantSubject;
+use Illuminate\Database\Seeder;
+
+class ApplicantSubjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $subjects = [
+            'Riyaziyyat',
+            'Fizika',
+            'Kimya',
+            'İnformatika',
+            'Coğrafiya',
+            'Tarix',
+            'Azərb. dili',
+            'Ədəbiyyat',
+            'Biologiya',
+            'Xarici dil',
+        ];
+
+        foreach ($subjects as $index => $title) {
+            ApplicantSubject::updateOrCreate(
+                [
+                    'title' => $title,
+                ],
+                [
+                    'identify' => ApplicantSubject::generateSlug($title),
+                    'order' => $index + 1,
+                ]
+            );
+        }
+    }
+}
